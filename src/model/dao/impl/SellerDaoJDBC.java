bc632bb835e8f579394354974f5232292d705dfe
Leandro <leandromkt.tr@gmail.com>
2025-12-09 10:03:10 -0300

    findById implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -7,53 +7,51 @@ import java.sql.SQLException;
 import java.util.List;
 
 import db.DB;
-import model.dao.DepartmentDao;
+import db.DbException;
 import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
-public class SellerDaoJDBC implements SellerDao{
+public class SellerDaoJDBC implements SellerDao {
 
 	private Connection conn;
-	
+
 	public SellerDaoJDBC(Connection conn) {
 		this.conn = conn;
 	}
-	
+
 	@Override
-	public void insert(DepartmentDao obj) {
+	public void insert(Seller obj) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
-	public void update(DepartmentDao obj) {
+	public void update(Seller obj) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void deleteById(Integer id) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 	@Override
-	public Seller findByIdSeller(Integer id) {
+	public Seller findById(Integer id) {
 		PreparedStatement st = null;
 		ResultSet rs = null;
 		try {
 			st = conn.prepareStatement(
-					"SELECT seller.*,department.Name as DepName "  
-					+ "FROM seller INNER JOIN department "  
-					+ "ON seller.DepartmentId = department.Id "  
-					+ "WHERE seller.Id = ?");
+					"SELECT seller.*,department.Name as DepName " + "FROM seller INNER JOIN department "
+							+ "ON seller.DepartmentId = department.Id " + "WHERE seller.Id = ?");
 			st.setInt(1, id);
 			rs = st.executeQuery();
-			if(rs.next()) {
+			if (rs.next()) {
 				Department dep = new Department();
 				dep.setId(rs.getInt("DepartmentId"));
-				dep.setName(rs.getName("DepName"));
+				dep.setNameString(rs.getString("DepName"));
 				Seller obj = new Seller();
 				obj.setId(rs.getInt("Id"));
 				obj.setNameString(rs.getString("Name"));
@@ -64,22 +62,16 @@ public class SellerDaoJDBC implements SellerDao{
 				return obj;
 			}
 			return null;
-		
-		}
+
+		} 
 		catch (SQLException e) {
-			throw new DbExcepetion(e.getMessage());
+			throw new DbException(e.getMessage());
 		}
+		
 		finally {
 			DB.closeStatement(st);
 			DB.closeResultSet(rs);
 		}
-		
-	}
-
-	@Override
-	public Department findById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
 	}
 
 	@Override
@@ -87,6 +79,4 @@ public class SellerDaoJDBC implements SellerDao{
 		// TODO Auto-generated method stub
 		return null;
 	}
-
-	
 }
